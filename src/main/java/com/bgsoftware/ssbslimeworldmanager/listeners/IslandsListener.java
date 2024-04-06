b63c34cd65f92653472f7101552fa7b3745fcc79
OmerBenGera <omerthepro126@gmail.com>
2024-04-06 13:37:54 +0300

    Fix the module teleporting the island more than once to his island

--- src/main/java/com/bgsoftware/ssbslimeworldmanager/listeners/IslandsListener.java
+++ src/main/java/com/bgsoftware/ssbslimeworldmanager/listeners/IslandsListener.java
@@ -12,6 +12,8 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class IslandsListener implements Listener {
 
     private final SlimeWorldModule module;
@@ -42,17 +44,21 @@ public class IslandsListener implements Listener {
             // If so, we teleport them to their island again.
             boolean teleportToIsland = defaultWorldTeleportLocation.equals(superiorPlayer.getLocation());
 
+            AtomicBoolean teleportedToIsland = new AtomicBoolean(false);
+
             // We want to load the worlds of the player's island.
             for (World.Environment environment : World.Environment.values()) {
                 if (isWorldGeneratedForIsland(island, environment))
                     module.getSlimeWorldsProvider().getSlimeWorldAsBukkitAsync(island.getUniqueId(), environment).whenComplete((world, error) -> {
-                        if (teleportToIsland)
+                        if (teleportToIsland && !teleportedToIsland.get()) {
                             superiorPlayer.teleport(island);
+                            teleportedToIsland.set(true);
+                        }
                     });
             }
 
             // Because it takes time for the worlds to load, we teleport them to spawn in the time being.
-            if (teleportToIsland)
+            if (teleportToIsland && !teleportedToIsland.get())
                 superiorPlayer.teleport(module.getPlugin().getGrid().getSpawnIsland());
         }
     }
